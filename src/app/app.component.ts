9d13c177e8340533ad235ff754b4b58761e6d55e
jjvg <jjvg_12@hotmail.com>
2018-04-10 01:36:47 -0400

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { LoginPage } from "../pages/login/login";
+import { PerfilPage } from '../pages/perfil/perfil';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = LoginPage;
+  pages: Array<{title: string, component: any,icon:string }>
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    this.pages=[
+      {title: 'Perfil', component: PerfilPage, icon:'contact'},
+      {title: 'Citas', component:'',icon:'calendar'},
+      {title: 'Mensajes',component:'',icon:'mail' },
+      {title: 'Reclamos',component:'',icon:'filing'},
+      {title: 'Solicitudes',component:'',icon:'paper-plane'},
+      {title: 'Servicios Recibidos',component:'',icon:'list-box'},
+      {title: 'Promociones',component:'',icon:'star'},
+      {title: 'Consejos para Ti',component:'',icon:'megaphone'}
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { ListaPeluqueriaComponent } from './lista-peluqueria/lista-peluqueria';
 import { ServrecomendadoComponent } from './servrecomendado/servrecomendado';
 import { ArtrecomendadoComponent } from './artrecomendado/artrecomendado';
+
 @NgModule({
 	declarations: [
     ListaPeluqueriaComponent,
     ServrecomendadoComponent,
-    ArtrecomendadoComponent],
+    ArtrecomendadoComponent,
+    ],
 	imports: [],
 	exports: [
 	
     ListaPeluqueriaComponent,
     ServrecomendadoComponent,
-    ArtrecomendadoComponent]
+    ArtrecomendadoComponent,
+    ]
 })
 export class ComponentsModule {}
