2dc4589cd1213211913c51c1d0b8e2420abf2530
jjvg <jjvg_12@hotmail.com>
2018-04-12 21:53:18 -0400

    funciona navegabilidad

--- src/pages/promociones/promociones.ts
+++ src/pages/promociones/promociones.ts
@@ -1,5 +1,7 @@
+import { SolicitudesPage } from './../solicitudes/solicitudes';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SolicitudPage } from '../solicitud/solicitud';
 
 /**
  * Generated class for the PromocionesPage page.
@@ -17,27 +19,33 @@ export class PromocionesPage {
 	items:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
    this.items=[{
-      "nombre":"Masaje Capilar",
+      "nombre":"Promocion 2x1",
       "content":"Existe una nueva tecnica para realizar masajes capilares a nuestros clientes enterate mas aqui",
-      "img":"assets/imgs/masajecapilar.jpg",
-      'duracion':'2 semanas'
+      "img":"assets/imgs/Promocion.jpg",
+      'duracion':'2 semanas',
+      'tipo':'promocion'
     },
   {
-    "nombre":"Rutina de belleza",
+    "nombre":"Promocion 2x1",
     "content":"En ocaciones la clave tener un bello rostro es seguir una rutina de belleza ",
-    "img":"assets/imgs/rutinas.jpg",
-    'duracion':'2 semanas'
+    "img":"assets/imgs/Promocion.jpg",
+    'duracion':'2 semanas',
+    'tipo':'promocion'
   },
   {
-    "nombre":"Exfoliante Natural",
+    "nombre":"Promocion 2x1",
     "content":"Perfecto para un rostro un poco grasoso, es importante utilizar exfoliantes para eliminar de nuestra piel las inpuresas que dia a dia recogemos en las calles ",
-    "img":"assets/imgs/exfoliante.png",
-    duracion: '2 semanas'
+    "img":"assets/imgs/Promocion.jpg",
+    'duracion': '2 semanas',
+    'tipo':'promocion'
   }];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PromocionesPage');
+  }
+  adquirirPromo(data){
+    this.navCtrl.push(SolicitudPage,data)
   }
 
 }
