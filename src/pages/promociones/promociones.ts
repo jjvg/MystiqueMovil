7f1d8893b6a72dfe031e3677c2949b7d3bc0f8be
jjvg <jjvg_12@hotmail.com>
2018-04-12 20:00:27 -0400

    commt

--- src/pages/promociones/promociones.ts
+++ src/pages/promociones/promociones.ts
@@ -14,8 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'promociones.html',
 })
 export class PromocionesPage {
-
+	items:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   this.items=[{
+      "nombre":"Masaje Capilar",
+      "content":"Existe una nueva tecnica para realizar masajes capilares a nuestros clientes enterate mas aqui",
+      "img":"assets/imgs/masajecapilar.jpg",
+      'duracion':'2 semanas'
+    },
+  {
+    "nombre":"Rutina de belleza",
+    "content":"En ocaciones la clave tener un bello rostro es seguir una rutina de belleza ",
+    "img":"assets/imgs/rutinas.jpg",
+    'duracion':'2 semanas'
+  },
+  {
+    "nombre":"Exfoliante Natural",
+    "content":"Perfecto para un rostro un poco grasoso, es importante utilizar exfoliantes para eliminar de nuestra piel las inpuresas que dia a dia recogemos en las calles ",
+    "img":"assets/imgs/exfoliante.png",
+    duracion: '2 semanas'
+  }];
   }
 
   ionViewDidLoad() {
