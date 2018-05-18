614fceebfa53260095c2b6c4a276271256efb042
sohecdy <sohecdyavila38@gmail.com>
2018-05-18 00:34:17 -0400

    cambios historial

--- src/pages/historial/historial.ts
+++ src/pages/historial/historial.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
+import {ServicioRProvider } from '../../providers/servicio-r/servicio-r';
+import {AuthProvider} from './../../providers/auth/auth';
+import {ClienteProvider} from '../../providers/cliente/cliente';
 
 /**
  * Generated class for the HistorialPage page.
@@ -15,11 +18,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HistorialPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  url_api:any;
+
+  serviciosr:{
+    id:number,
+    nombre:string,
+    apellido:string,
+    cedula:string,
+    telefono:number,
+    direccion:string,
+    tipo_cliente:string,
+    ordenes:Array<{
+      id:string,
+      id_solicitud,
+      estado:string}>
+  }
+  ordenes_en_espera:any[];
+  ordenes_realizadas:any[];
+  ordenes_calificadas:any[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+    public serviciosrProvider: ServicioRProvider,
+    public authService: AuthProvider,
+    public clienteProvider: ClienteProvider){
+
+      this.serviciosr={
+        id:0,
+        nombre:'',
+        apellido:'',
+        cedula:'',
+        telefono:0,
+        direccion:'',
+        tipo_cliente:'',
+        ordenes:[]
+      }
+      this.ordenes_en_espera=[];
+      this.ordenes_realizadas=[];
+      this.ordenes_calificadas=[];
+
+ 
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HistorialPage');
-  }
+    this.url_api= this.authService.ApiUrl();
+    console.log(this.serviciosr);
+    this.getOrdenes(); }
+  
 
+  llenarArray(){
+  this.ordenes_en_espera=[];
+  this.ordenes_realizadas=[];
+  this.ordenes_calificadas=[];
+    console.log(this.serviciosr.ordenes);
+
+  for (let i = 0; i < this.serviciosr.ordenes.length; i++) {
+    if(this.serviciosr.ordenes[i].estado==='R'){
+      this.ordenes_realizadas.push(this.serviciosr.ordenes[i]);
+    }else{
+      if(this.serviciosr.ordenes[i].estado==='E'){
+        this.ordenes_en_espera.push(this.serviciosr.ordenes[i].estado);
+      }
+    }
+  }
+  console.log(this.ordenes_en_espera);
+  console.log(this.ordenes_realizadas);
+}
+getOrdenes(){
+  this.serviciosrProvider.getServiciosR(this.clienteProvider.getCliente().id).subscribe(
+    (data)=>{
+      this.serviciosr=data['data'];
+      console.log(this.serviciosr)
+      this.llenarArray();  
+    },(error)=>{console.log(error)} 
+  );}
 }
